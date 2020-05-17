23c971b3aed197d76b529904aec02bcd20d483de
Captain Cobbs <egamezhq@gmail.com>
2020-05-17 03:17:26 -0600

    Added the InputManager, which manages key and mouse controls

---
+++ MapDescriptorTest/Entity/InputManager.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapDescriptorTest.Entity
+{
+    public static class InputManager
+    {
+        public static KeyboardState KeyboardState { get; private set; }
+        public static MouseState MouseState { get; private set; }
+
+        public static void Update()
+        {
+            KeyboardState = Keyboard.GetState();
+            MouseState = Mouse.GetState();
+        }
+    }
+}
