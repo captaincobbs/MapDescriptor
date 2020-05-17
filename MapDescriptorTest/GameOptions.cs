c797c6b0291d2d7f195a57f1abb838ccf2b01fe1
Captain Cobbs <egamezhq@gmail.com>
2020-05-17 03:16:13 -0600

    Added GameOptions for MapSize, InputDelay, FrameCounter, and IntertiaFactor

--- MapDescriptorTest/GameOptions.cs
+++ MapDescriptorTest/GameOptions.cs
@@ -9,5 +9,9 @@ namespace MapDescriptorTest
     public static class GameOptions
     {
         public static int TileSize = 128;
+        public static int MapSize = 20;
+        public static int InputDelay = 7;
+        public static int FrameCounter = 0;
+        public static float InertiaFactor = 0.15f;
     }
 }
