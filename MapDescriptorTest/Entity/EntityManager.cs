7ede98c137170cb2d4b4ad5457b5011cdbe795d7
Captain Cobbs <egamezhq@gmail.com>
2020-05-17 22:05:17 -0600

    Added useful comments, renamed MovePlayer to HandleMovement

--- MapDescriptorTest/Entity/EntityManager.cs
+++ MapDescriptorTest/Entity/EntityManager.cs
@@ -1,17 +1,22 @@
-﻿using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-using System;
+﻿using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MapDescriptorTest.Entity
 {
+    /// <summary>
+    /// A manager that constantly updates and draws changes in entities
+    /// </summary>
     public class EntityManager
     {
+        /// <summary>
+        /// Collection of all entities
+        /// </summary>
         public List<IHasEntity> Entities { get; set; } = new List<IHasEntity>();
 
+        /// <summary>
+        /// Goes through every entity and redraws them as needed
+        /// </summary>
+        /// <param name="spriteBatch">Sprite Batch to add updated sprites onto</param>
         public void Draw(SpriteBatch spriteBatch)
         {
             foreach (IHasEntity entity in Entities)
@@ -21,6 +26,9 @@ namespace MapDescriptorTest.Entity
             }
         }
 
+        /// <summary>
+        /// Updates all entities every frame
+        /// </summary>
         public void Update()
         {
             foreach (IHasEntity entity in Entities)
