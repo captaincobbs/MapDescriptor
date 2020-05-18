8e0db04feee3ba716570166e601778ebd601ae2d
Captain Cobbs <egamezhq@gmail.com>
2020-05-17 21:58:03 -0600

    Added BackgroundColor, made Tile and Map size constant, added Comments

--- MapDescriptorTest/GameOptions.cs
+++ MapDescriptorTest/GameOptions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,29 @@ namespace MapDescriptorTest
 {
     public static class GameOptions
     {
-        public static int TileSize = 128;
-        public static int MapSize = 20;
-        public static int InputDelay = 7;
+        /// <summary>
+        /// Pixel size of each world grid tile
+        /// </summary>
+        public const int TileSize = 128;
+        /// <summary>
+        /// Size^2 of the world map
+        /// </summary>
+        public const int MapSize = 20;
+        /// <summary>
+        /// Delay for accepting input from the keyboard for player keyboard input
+        /// </summary>
+        public const int InputDelay = 6;
+        /// <summary>
+        /// Frames counted since last reached InputDelay
+        /// </summary>
         public static int FrameCounter = 0;
-        public static float InertiaFactor = 0.15f;
+        /// <summary>
+        /// Inertia speed of the camera
+        /// </summary>
+        public const float InertiaFactor = 0.15f;
+        /// <summary>
+        /// Color of the window background
+        /// </summary>
+        public static Color BackgroundColor = Color.Black;
     }
 }
