d47599f7a9345bd18ca63c2e87574f2c99b33ef8
AutonomyCOG <egamezhq@gmail.com>
2020-05-19 21:11:18 -0600

    Added Common

---
+++ MapDescriptorTest/Common.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapDescriptorTest
+{
+    /// <summary>
+    /// Class of common functions I don't like doing
+    /// </summary>
+    public static class Common
+    {
+        /// <summary>
+        /// Forces a float to stay within the range specified.
+        /// </summary>
+        /// <param name="value">float to check is within the range</param>
+        /// <param name="min">Minimum wanted value</param>
+        /// <param name="max">Maximum wanted value</param>
+        /// <returns>Returns value if within max and min, otherwise returns min or max</returns>
+        public static float ContainInRange(float value, float min, float max)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
+    }
+}
